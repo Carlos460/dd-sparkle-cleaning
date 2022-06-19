318ffdb33533309547d04d417d48e939041ef105
Noureddine Sidi <52947081+Noureddine-prg@users.noreply.github.com>
2022-06-19 00:40:37 -0400

    Added About Page

---
+++ pages/about.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Navbar from "@includes/Navbar";
+import AboutUs from "@sections/AboutUs";
+
+import { Row, Col } from "antd";
+
+import * as S from "../styles/Home.styles";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Sparkle Cleaning</title>
+        <meta name="description" content="DD Sparkle Cleaning" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Row justify="center">
+          <Col xs={22} sm={18} lg={20} xxl={14}>
+            <Navbar />
+            <AboutUs />
+          </Col>
+        </Row>
+      </main>
+    </>
+  );
+};
+
+export default About;
