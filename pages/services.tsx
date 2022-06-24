3429b231d406a3ef1c01753983ef90cc59edfefa
Carlos Flores <carlosfloresalex@gmail.com>
2022-06-23 21:54:13 -0400

    Added services page, basic layout

---
+++ pages/services.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Navbar from '@includes/Navbar';
+import Footer from '@includes/Footer';
+
+import { Row, Col } from 'antd';
+
+// Sections
+import OurServices from '@sections/OurServices';
+
+// Components
+import * as S from '@styles/Sparkle.styles';
+
+const Services: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Sparkle Cleaning</title>
+        <meta name="description" content="DD Sparkle Cleaning" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <S.Section>
+          <Navbar theme="pink" />
+          <S.Wave style={{ transform: 'scaleY(-1)', top: 70 }} />
+          <Row justify="center">
+            <Col xs={22} sm={18} lg={20} xxl={14}>
+              <OurServices />
+            </Col>
+          </Row>
+        </S.Section>
+        <Footer />
+      </main>
+    </>
+  );
+};
+
+export default Services;
